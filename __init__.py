3c7fa24ad2c49a28703c4f72c18abbf4f17fa614
jake <jakemillerdev@gmail.com>
2022-05-16 21:08:42 -0700

    Added enum property to determine whether multi-group will have each object move to desired coordinate, or the average group location moved to that coordinate.

--- __init__.py
+++ __init__.py
@@ -27,7 +27,8 @@ class COORDINATE_PROPERTIES(bpy.types.PropertyGroup):
     x: bpy.props.FloatProperty(name="X", default= 0)
     y: bpy.props.FloatProperty(name="Y", default= 0)
     z: bpy.props.FloatProperty(name="Z", default= 0)
-
+class MULTI_SELECT_PROPERTIES(bpy.types.PropertyGroup):
+    move_by_type: bpy.props.EnumProperty(name = "Move...", description="Move all objects to coordinate, or move group average location to coordinate.", items=[("All to Coordinate", "All to Coordinate", "All to Coordinate"), ("Average to Coordinate", "Average to Coordinate", "Average to Coordinate")])
   
 
 
@@ -51,7 +52,11 @@ class COORDINATE_PANEL_PT_panel(bpy.types.Panel):
         layout.prop(coordinates, "x")
         layout.prop(coordinates, "y")
         layout.prop(coordinates, "z")
-
+        # IF MULTIPLE OBJECTS SELECTED
+        if len(context.selected_objects) > 1:
+            row = layout.row()
+            row.label(text= "Multiple selected")
+            layout.prop(scene.multi_select_props, "move_by_type")
         row = layout.row()
         row.operator(COORDINATE_PLACEMENT_OPERATOR.bl_idname, icon="OUTLINER_OB_EMPTY")
        
@@ -59,11 +64,13 @@ class COORDINATE_PLACEMENT_OPERATOR(bpy.types.Operator):
     bl_idname = "object.place_at_coords"
     bl_label = "Place at Coordinates"
     bl_description = "Enter object location manually."
-
     @classmethod
     def poll(self, context):
         return context.object is not None
     def execute(self, context):
+        # Determine if solo or group
+        if len(context.selected_objects) > 1:
+            mean = self.getGroupMean(context.selected_objects)
         scene = context.scene
         coordinates = scene.object_coordinates
         object = context.object
@@ -71,6 +78,20 @@ class COORDINATE_PLACEMENT_OPERATOR(bpy.types.Operator):
         object.location.y = coordinates.y
         object.location.z = coordinates.z
         return {"FINISHED"}
+    def getGroupMean(self, objects):
+        vector = [0,0,0]
+        for i in objects:
+            vector[0] += i.location.x
+            vector[1] += i.location.y
+            vector[2] += i.location.z
+        length = len(objects)
+        # divide total x,y,z coord by length of objects
+        for indx, i in enumerate(vector):
+            vector[indx] = i / length 
+        return vector
+    def getObjectOffset(self, object, mean):
+        return [mean[0] - object.location.x, mean[1] - object.location.y, mean[2] - object.location.z]
+
 
 class RESET_COORD_OPERATOR(bpy.types.Operator):
     bl_idname = "object.reset_coordinates"
@@ -93,17 +114,22 @@ toRegister = [
     COORDINATE_PROPERTIES,
     COORDINATE_PLACEMENT_OPERATOR,
     COORDINATE_PANEL_PT_panel,
-    RESET_COORD_OPERATOR
+    RESET_COORD_OPERATOR,
+    MULTI_SELECT_PROPERTIES
 ]
 def register():
     for i in toRegister:
         bpy.utils.register_class(i)
     bpy.types.Scene.object_coordinates = bpy.props.PointerProperty(type = COORDINATE_PROPERTIES)
+    bpy.types.Scene.multi_select_props = bpy.props.PointerProperty(type = MULTI_SELECT_PROPERTIES)
+
 
 def unregister():
     for i in toRegister:
         bpy.utils.unregister_class(i)
     del bpy.types.Scene.object_coordinates
+    del bpy.types.Scene.multi_select_props
+
 
 
 if __name__ == "__main__":
